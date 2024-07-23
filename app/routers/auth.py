b16eccc854ac11965cca421d395ed6f50c1eda07
miraj <mirajrambhiya@gmail.com>
2024-07-23 12:22:41 +0530

    Changed the status code in user login for invalid credentials

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -12,10 +12,10 @@ def user_login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Sess
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code= status.HTTP_403_FORBIDDEN, detail= "Invalid Credentials")
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail= "Invalid Credentials")
     
     if not utils.verify_password(user_credentials.password, user.password):
-        raise HTTPException(status_code= status.HTTP_403_FORBIDDEN, detail= "Invalid Credentials")
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail= "Invalid Credentials")
     
     access_token = oath2.create_access_token({"user_id": user.id})
 
